907893ae92000a836e84b49788147ef62e2ff95a
Mike Hawley <mhawley1230@gmail.com>
2018-04-02 14:04:17 -0400

    added Body component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
+import Body from './Body.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Body />
       </div>
     );
   }
