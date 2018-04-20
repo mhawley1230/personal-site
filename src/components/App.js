00a353eac4f5fc67b6b3c7bbe00c88ddcb5d3c44
Mike Hawley <mhawley1230@gmail.com>
2018-04-20 13:55:15 -0400

    added navbar to top of app

--- src/components/App.js
+++ src/components/App.js
@@ -9,29 +9,25 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className='title-name'>
-          <p>Michael Hawley</p>
-        </div>
         <Router>
-          <div className="App">
-            <ul>
-              <li>
-                <Link to='/' component={Home}>Home</Link>
-              </li>
-              <li>
-                <Link to='/blog' component={Blog}>Blog</Link>
-              </li>
-              <li>
-                <Link to='/resume' component={Resume}>Resume</Link>
-              </li>
-            </ul>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/blog' component={Blog} />
-              <Route path='/resume' component={Resume} />
-              <Route component={NoMatch} />
-            </Switch>
-          </div>
+          <nav>
+            <div class="nav-wrapper">
+              <Link class='brand-logo' to='/' component={Home}>Home</Link>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li></li>
+                <li><Link to='/blog' component={Blog}>Blog</Link></li>
+                <li><Link to='/resume' component={Resume}>Resume</Link></li>
+              </ul>
+            </div>
+            <div>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/blog' component={Blog} />
+                <Route path='/resume' component={Resume} />
+                <Route component={NoMatch} />
+              </Switch>
+            </div>
+          </nav>
         </Router>
       </div>
     );
