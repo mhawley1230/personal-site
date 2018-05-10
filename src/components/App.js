38ea70979334fbafc740cb7b3a57aafebd67d5ed
Mike Hawley <mhawley1230@gmail.com>
2018-05-10 11:49:36 -0400

    started using materialize, don't worry about mobile for now

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,34 @@ import Blog from './pages/Blog.js';
 import NoMatch from './pages/NoMatch.js';
 import Resume from './pages/Resume.js';
 
+// import '../../js/materialize.js';
+
 class App extends Component {
   render() {
     return (
       <div>
         <Router>
-          <nav>
-            <div class="nav-wrapper">
-              <Link class='brand-logo' to='/' component={Home}>Home</Link>
-              <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li></li>
-                <li><Link to='/blog' component={Blog}>Blog</Link></li>
-                <li><Link to='/resume' component={Resume}>Resume</Link></li>
-              </ul>
-            </div>
+          <div>
+            <nav>
+              <div class="nav-wrapper">
+                <a href="#!" class="brand-logo">Logo</a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                  <li><a href="sass.html">Sass</a></li>
+                  <li><a href="badges.html">Components</a></li>
+                  <li><a href="collapsible.html">Javascript</a></li>
+                  <li><a href="mobile.html">Mobile</a></li>
+                </ul>
+              </div>
+            </nav>
+
+            <ul class="sidenav" id="mobile-demo">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li>
+              <li><a href="collapsible.html">Javascript</a></li>
+              <li><a href="mobile.html">Mobile</a></li>
+            </ul>
+
             <div>
               <Switch>
                 <Route exact path='/' component={Home} />
@@ -27,11 +41,10 @@ class App extends Component {
                 <Route component={NoMatch} />
               </Switch>
             </div>
-          </nav>
+          </div>
         </Router>
       </div>
     );
   }
 }
-
 export default App;
